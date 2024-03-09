ec7d228c053ea8517419ef4aaa87e10bc57f731e
Roman Milkov <mir3227@mail.ru>
2024-03-09 22:34:20 +0500

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,12 @@
 package ru.netology;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBook {
+    List<String> phoneBook = new ArrayList<>();
+
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
 }
--- src/test/java/TestPhoneBook.java
+++ src/test/java/TestPhoneBook.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPhoneBook {
+    @Test
+    public void testAddition(){
+        PhoneBook phoneBook = new PhoneBook();
+        int contactCount = phoneBook.add("Roman", "77015785214");
+        assertEquals(1, contactCount);
+    }
 }
