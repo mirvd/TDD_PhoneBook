fab86a986a6a3df83a325731948cfb89272de41c
Roman Milkov <mir3227@mail.ru>
2024-03-09 22:36:26 +0500

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class PhoneBook {
     List<String> phoneBook = new ArrayList<>();
 
-    public int add(String name, String phoneNumber) {
-        return 0;
+    public int add(String name, String phoneNumber){
+        String contact = name + " - " + phoneNumber;
+        if(!phoneBook.contains(contact)) {
+            phoneBook.add(contact);
+        }
+        return phoneBook.size();
     }
 }
